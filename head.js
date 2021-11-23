1362f2a9c96db173e2b665d3f1389851f59bc613
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-22 21:14:17 -0600

    linting head.js

--- head.js
+++ head.js
@@ -1,14 +1,13 @@
-let array = [];
-const head = function(array){
+const head = function(array) {
   return array[0];
-}
-const assertEqual = function(a, b){
-  if(a === b){
+};
+const assertEqual = function(a, b) {
+  if (a === b) {
     console.log('Passed');
   } else {
     console.log('Failed');
   }
-}
+};
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
