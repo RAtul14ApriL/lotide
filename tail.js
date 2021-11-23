c5ba9f0b5c5e6ae29add488b69560e7b822e2872
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-22 21:23:53 -0600

    add tail

--- tail.js
+++ tail.js
@@ -1,16 +1,18 @@
-let array = [];
-const tail = function(array){
-  array.pop()
+const tail = function(array) {
+  array = array.slice(1);
   return array;
-}
-const assertEqual = function(a, b){
-  if(a === b){
+};
+const assertEqual = function(a, b) {
+  if (a === b) {
     console.log('Passed');
   } else {
     console.log('Failed');
   }
-}
+};
+assertEqual(tail([5, 6, 7]), [6, 7]);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+assertEqual(tail([5, 6, 7]), 6);
 
-assertEqual(head([5, 6, 7]), [6, 7]);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertEqual(head([5, 6, 7]), 6);
+const arr = [1, 2, 3, 4];
+tail(arr);
+assertEqual(arr.length, 4);
