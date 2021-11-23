c9b215b0dbff8adcbf060845399bb848e8d31fe7
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-22 22:27:34 -0600

    Added undefined argument

--- head.js
+++ head.js
@@ -4,11 +4,14 @@ const head = function(array) {
 const assertEqual = function(a, b) {
   if (a === b) {
     console.log('Passed');
+  } else if (a === undefined) {
+    console.log('undefined');
   } else {
-    console.log('Failed');
+    console.log('False');
   }
 };
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5, 6, 7]), 6);
+assertEqual(head([]), 6);
