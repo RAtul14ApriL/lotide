b194af87a9ac6f3f33da201dabb360f404e52285
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-22 20:23:57 -0600

    Finding the first item of the array

---
+++ head.js
@@ -0,0 +1,15 @@
+let array = [];
+const head = function(array){
+  return array[0];
+}
+const assertEqual = function(a, b){
+  if(a === b){
+    console.log('Passed');
+  } else {
+    console.log('Failed');
+  }
+}
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5, 6, 7]), 6);
