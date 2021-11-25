05f1e6699d5f9288c0f8e5f619aa87d429856aba
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-24 21:59:38 -0600

    Finding an object key by matching value

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value){
+  let output = "";
+for(let key in object){
+  if(object[value] === object.key)
+  output = key;
+  }
+  return output;
+}
+
+//*****************Test codes *****************************
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
