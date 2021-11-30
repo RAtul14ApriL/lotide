0823f13f52cbb7b5b56bcd86cb12140860c517a7
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-29 21:30:30 -0600

    Adding index.js to export all the functions using a single file

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const countLetter = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
 const head = require('./head');
+const letterPositions = require('./letterPositions');
+const loopyLighthouse = require('./loopyLighthouse');
+const map = require('./map');
 const middle = require('./middle');
+const passwordObfuscator = require('./passwordObfuscator');
+const pigLatin = require('./pigLatin');
 const tail = require('./tail');
 
 module.exports = {
-head: head,
-middle: middle,
-tail: tail
+  countLetter: countLetter,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  letterPositions: letterPositions,
+  loopyLighthouse: loopyLighthouse,
+  map: map,
+  middle: middle,
+  passwordObfuscator: passwordObfuscator,
+  pigLatin: pigLatin,
+  tail: tail
 }
